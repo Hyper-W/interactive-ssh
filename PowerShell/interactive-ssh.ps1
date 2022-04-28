b9302383f6c71372972a5f8d86b8d3c9b5289158
Hyper-W <101781346+Hyper-W@users.noreply.github.com>
2022-04-28 16:06:36 +0900

    Bug fixes when one host(PowerShell).

--- PowerShell/interactive-ssh.ps1
+++ PowerShell/interactive-ssh.ps1
@@ -1,7 +1,6 @@
 Get-Content ~\.ssh\config |  Select-String -Pattern "^Host " | Select-String -NotMatch "github" | foreach-object{ $_ -Replace "Host " , ""}| Out-String -Stream | Select-String '.*' | Select-Object LineNumber, Line | Format-Table
-$SSH_HOSTS = Get-Content ~\.ssh\config |  Select-String -Pattern "^Host " | Select-String -NotMatch "github" | foreach-object{ $_ -Replace "Host " , ""}
+$SSH_HOSTS = @(Get-Content ~\.ssh\config |  Select-String -Pattern "^Host " | Select-String -NotMatch "github" | foreach-object{ $_ -Replace "Host " , ""})
 [int]$SSH_Num = Read-Host  "Select Connect Host Number"
-
 
 if($SSH_HOSTS[$SSH_Num-1] -eq $null){
     Write-Output "An invalid value was entered."
